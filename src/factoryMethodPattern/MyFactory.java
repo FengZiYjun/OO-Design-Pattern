9817ba797c9d85862d5cdc3a5bafd77945c88dbe
Feng ZiYjun <writerphone@163.com>
2017-05-23 09:33:48 +0800

    add

---
+++ src/factoryMethodPattern/MyFactory.java
@@ -0,0 +1,26 @@
+package factoryMethodPattern;
+
+public class MyFactory implements AbstractFactory {
+
+	public MyFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public AbstractProduct puduce() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void afterservice() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
